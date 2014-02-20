3fb71f18e8b369fef00f04d3cb83d26cf8f04348
Robert McGibbon <rmcgibbo@gmail.com>
2014-02-19 22:48:21 -0800

    version

--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
 from setuptools import setup
+__version__ = '0.1.0'
 
 setup(name='bibpy',
+      version=__version__,
       packages=['bibpy'],
       include_package_data=True,
       package_data={'': ['*.json']},
